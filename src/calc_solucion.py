ad6e1b460a7e67a655f9c1a3b4e9cb03af85887b
ArgiesDario <argiesdario@gmail.com>
2017-12-28 11:02:03 -0800

    Validador de soluciones para archivos finalizado
    Se alcanzo la puntuacion de 63790.25 con el archivo ganador, cual segun la pagina tiene una puntuacion de 63790.55. Habiendo una diferencia de 0.30 por decimales, se considera correcto

--- src/calc_solucion.py
+++ src/calc_solucion.py
@@ -22,7 +22,7 @@ travel = obtener_archivos.get_archivo_solucion(path=r"..\info\solucion_ganadora.
 
 #Contadores utilizados para obtener descuentos con secuencias de agencias o acumulacion de km en el caso de la agencia D
 cont_age_a = 0
-cont_age_bc = 0
+cont_age_bc = False
 cont_age_d = 0
 #Descuentos segun agencia
 desc_a = 0.65   # %
@@ -30,9 +30,10 @@ desc_b = 0.85   # %
 desc_c = 0.80   # %
 desc_d = 15     # $
 acum_d = 0      # KM
-it = 0
 #Precio acumulado
 precio_acum = 0 # $
+#Debug
+it = 0
 
 #Recorro la lista de ciudades
 for item in travel:
@@ -47,24 +48,24 @@ for item in travel:
     distancia = calc_distancia.dist_ciudades(ciudad_desde=ciudad_desde, ciudad_hasta=ciudad_hasta)
     precio = distancia * 0.01
 
-    #Armar secuencia
+    #Armar secuencia: A-A-A y B-C
     if item["agencia"].upper() == "A":
-        #Agregar A, reiniciar C
+        #Agregar A, reiniciar BC
         cont_age_a += 1
-        cont_age_bc = 0
+        cont_age_bc = False
     elif item["agencia"].upper() == "B":
         #Agregar BC, reiniciar A
-        cont_age_bc += 1
+        cont_age_bc = True
         cont_age_a = 0
     elif item["agencia"].upper() == "C":
         #Reiniciar contadores
         cont_age_a = 0
-        #cont_age_bc se reinicia mas adelante
+        #cont_age_bc no se reinicia aca ya que perderia la secuencia B-C
     elif item["agencia"].upper() == "D":
         #Agregar la distancia de viaje en agencia D. Reiniciar contadores
         cont_age_d += distancia
         cont_age_a = 0
-        cont_age_bc = 0
+        cont_age_bc = False
 
     #Armar descuentos
     if item["agencia"].upper() == "A" and cont_age_a == 3:
@@ -74,10 +75,10 @@ for item in travel:
     if item["agencia"].upper() == "B" and distancia > 200:
         #Si es agencia B y la distancia es mayor a 200, aplico el 15% de descuento
         precio = precio * desc_b
-    if item["agencia"].upper() == "C" and cont_age_bc == 1:
+    if item["agencia"].upper() == "C" and cont_age_bc:
         #Si la agencia es C y la anterior es B, aplico 20% de descuento al vuelo C
         precio = precio * desc_c
-        cont_age_bc = 0 #Aca se reinicia el contador
+        cont_age_bc = False #Aca se reinicia el contador
     if item["agencia"].upper() == "D":
         acum_d += distancia
 
